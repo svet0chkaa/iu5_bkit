287e564568ee3729012bae289114f3879b0c330b
IKPulMatEge <114401938+IKPulMatEge@users.noreply.github.com>
2022-12-09 23:42:50 +0300

    Delete lab_3 directory

--- lab_3/lab_python_fp/gen_random.py
+++
@@ -1,8 +0,0 @@
-import random
-def gen_random(num_count, begin, end):
-    assert num_count > 0
-    temp = []
-    for i in range(num_count):
-        temp.append(random.randrange(begin, end + 1))
-    print(*temp, sep = ", ")
-    return temp
